ecb66e925396bd1f71b6f975fed6cec70f0f74a8
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2021-08-11 01:05:54 +0300

    Cryptocurrency Wallet & Miner

---
+++ Miner.py
@@ -0,0 +1,11 @@
+#Miner
+import SocketUtils
+
+wallet_list = ['localhost']
+def minerServer(my_ip, wallet_list):
+    # Open Server Connection
+    # Rec'v 2 transactions
+    # Collect into block
+    # Find nonce
+    # Send that block to each in walle_list
+    return False
